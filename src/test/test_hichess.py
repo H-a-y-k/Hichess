5315be61984f15fe340e061ad9994c7f18d9c0ae
H-a-y-K <agokugtw@gmail.com>
2020-01-01 19:41:27 +0400

    The game logic is ready

--- src/test/test_hichess.py
+++ src/test/test_hichess.py
@@ -1,8 +1,11 @@
 import unittest
+
 import hichess.hichess as hichess
-from PySide2.QtWidgets import QApplication, QWidget
-import sys
 import chess
+import chess.pgn
+
+from PySide2.QtWidgets import QApplication
+import sys
 
 
 class SquareWidgetTestCase(unittest.TestCase):
@@ -28,13 +31,49 @@ class PieceWidgetTestCase(unittest.TestCase):
 
     def testTransformation(self):
         w = hichess.PieceWidget(chess.A2, chess.Piece(chess.PAWN, chess.WHITE))
-        w.transformInto(chess.Piece(chess.PAWN, chess.WHITE))
+        w.transformInto(chess.PAWN)
         self.assertEqual(w.piece, chess.Piece(chess.PAWN, chess.WHITE))
 
 
-class BoardLayoutTestCase(unittest.TestCase):
-    def testDeleteWidgets(self):
-        pass
+class BoardWidgetTestCase(unittest.TestCase):
+    def testPieceWidgetAt(self):
+        square = chess.A5
+        pieceWidget = hichess.PieceWidget(square, chess.Piece(chess.PAWN, chess.WHITE))
+        boardWidget = hichess.BoardWidget()
+        boardWidget.loadPieceWidget(pieceWidget)
+        self.assertTrue(pieceWidget, boardWidget.pieceWidgetAt(square))
+
+    def _playGame(self, game: chess.pgn.Game, boardWidget):
+        for move in game.mainline_moves():
+            boardWidget.pushPieceWidget(move.to_square, boardWidget.pieceWidgetAt(move.from_square))
+
+    def _testBoardState(self, boardWidget):
+        for w in filter(lambda w: isinstance(w, hichess.PieceWidget), boardWidget.layout().widgets):
+            self.assertTrue(w.piece, boardWidget.board.piece_at(w.square))
+
+    def testGameplay1(self):
+        boardWidget = hichess.BoardWidget()
+        with open(r"test\games\long_game1.pgn", "r", encoding="utf-8-sig") as pgn:
+            self._playGame(chess.pgn.read_game(pgn), boardWidget)
+            self._testBoardState(boardWidget)
+
+    def testGameplay2(self):
+        boardWidget = hichess.BoardWidget()
+        with open(r"test\games\long_game2.pgn", "r", encoding="utf-8-sig") as pgn:
+            self._playGame(chess.pgn.read_game(pgn), boardWidget)
+            self._testBoardState(boardWidget)
+
+    def testGameplay3(self):
+        boardWidget = hichess.BoardWidget()
+        with open(r"test\games\long_game3.pgn", "r", encoding="utf-8-sig") as pgn:
+            self._playGame(chess.pgn.read_game(pgn), boardWidget)
+            self._testBoardState(boardWidget)
+
+    def testGameplay4(self):
+        boardWidget = hichess.BoardWidget()
+        with open(r"test\games\long_game4.pgn", "r", encoding="utf-8-sig") as pgn:
+            self._playGame(chess.pgn.read_game(pgn), boardWidget)
+            self._testBoardState(boardWidget)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
